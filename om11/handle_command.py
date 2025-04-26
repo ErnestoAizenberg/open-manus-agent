9c69d571cec1bbc1ce30d9606a3f5e7a8343a176
Ernesto <sereernest@gmail.com>
2025-04-26 12:54:08 +0300

    Update handle_command.py
    Async correction

--- om11/handle_command.py
+++ om11/handle_command.py
@@ -2,9 +2,9 @@ from task_registry import task_registry
 from execute_task_chain import execute_task_chain
 from llm.ask_gpt_chain import ask_gpt_chain
 
-def handle_command(user_input):
+async def handle_command(user_input):
     print(f"📥 Команда получена: {user_input}")
     task_chain = ask_gpt_chain(user_input)
     print(f"📦 Сгенерирован TaskChain: {task_chain}")
-    result = execute_task_chain(task_chain, task_registry)
+    result = await execute_task_chain(task_chain, task_registry)
     return result
