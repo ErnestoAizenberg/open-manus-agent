c3d80c77adac131dcb5ea5878f78476440a7fda4
Ernesto Aizenberg <sereernest@gmail.com>
2025-05-14 00:21:12 +0300

    corrected

--- om11/api.py
+++ om11/api.py
@@ -28,17 +28,55 @@ class APIHandler:
             "/api/execute_command/", self.execute_command, methods=["GET"]
         )
         self.app.add_api_route(
+            "/api/start_browser/", self.start_browser_route, methods=["POST"]
+        )
+        self.app.add_api_route(
             "/api/close_browser/", self.close_browser, methods=["POST"]
         )
 
-    async def get_or_create_browser_manager(self, user_uuid: str, headless: bool):
+    async def get_browser_manager(self, user_uuid: str, headless: bool):
         if user_uuid in self.user_browsers:
             return self.user_browsers[user_uuid]
         else:
+            return None
+
+    async def set_browser_manager(
+        self,
+        user_uuid,
+        headless: bool,
+        ws_url,
+    ):
+        browser_manager = BrowserManager()
+        browser_manager.connect_ws(ws_url)
+        self.user_browsers[user_uuid] = browser_manager
+        return self.user_browsers[user_uuid]
+
+    async def start_browser_route(
+        self,
+        ws_url: str = Query(..., description="Websoket url for running user browser"),
+        user_uuid: str = Query(..., description="<User uuid"),
+    ) -> JSONResponse:
+        try:
             browser_manager = BrowserManager()
-            await browser_manager.init_browser(headless=headless)
-            self.user_browsers[user_uuid] = browser_manager
-            return self.user_browsers[user_uuid]
+            browser_manager.connect_ws(ws_url)
+            if browser_manager._browser:
+                self.user_browsers[user_uuid] = browser_manager
+                return JSONResponse(
+                    content={"success": "Browser connected"}, status_code=200
+                )
+            else:
+                self.logger.info("Cannot set up user browser")
+                return JSONResponse(
+                    content={
+                        "error": "Browser is not connected, please check data and try again"
+                    },
+                    status_code=500,
+                )
+        except Exception as e:
+            self.logger.error(
+                f"Error occured while setting up user browser_manager instance: {str(e)}"
+            )
+            return JSONResponse(content={"error": "An error occured"}, status_code=500)
 
     async def execute_command(
         self,
@@ -49,9 +87,13 @@ class APIHandler:
         if not message or not user_uuid:
             raise HTTPException(status_code=400, detail="Missing required parameters")
         try:
-            browser_manager_instance = await self.get_or_create_browser_manager(
+            browser_manager_instance = await self.get_browser_manager(
                 user_uuid, headless
             )
+            if not browser_manager_instance:
+                JSONResponse(
+                    content={"error": "Browser is not connected"}, status_code=400
+                )
 
             tasks = Tasks(
                 browser_manager=browser_manager_instance,
