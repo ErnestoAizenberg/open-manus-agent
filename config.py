3a8700c0700276131b2864abf060d0718912d232
Ваше Имя <sereernest@gmail.com>
2025-08-11 18:47:51 +0300

    fix: use only one source for configuration (.env)

--- config.py
+++ config.py
@@ -1,29 +1,15 @@
-# config.py
 import os
-
 from dotenv import load_dotenv
 
-from tools.args_parser import parse_arguments
-
+# Load environment variables from .env file
 load_dotenv("instance/.env")
-args = parse_arguments()
-
-
-def get_input(prompt, default=None):
-    user_input = input(f"{prompt} (default: {default}): ")
-    return user_input if user_input else default
 
 
 class Config:
     """Basic Flask config."""
-
-    SECRET_KEY = args.secret_key or get_input(
-        "Введите секретный ключ", "your_very_secret_key_here"
-    )
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_very_secret_key_here")
     DEBUG = os.getenv("FLASK_DEBUG", "false").lower() in ["true", "1", "t"]
-    SERVER_ADDRESS = args.server_address or get_input(
-        ">>адрес сервера", "https://example.com"
-    )
+    SERVER_ADDRESS = os.getenv("SERVER_ADDRESS", "https://example.com")
 
     def get(self, key, default=None):
         return getattr(self, key, default)
@@ -31,10 +17,9 @@ class Config:
 
 class RedisConfig:
     """Redis configuration settings."""
-
-    HOST = args.redis_host or get_input("Введите хост Redis", "localhost")
-    PORT = args.redis_port or int(get_input("Введите порт Redis", 6379))
-    DB = args.redis_db or int(get_input("Введите номер базы данных Redis", 0))
+    HOST = os.getenv("REDIS_HOST", "localhost")
+    PORT = int(os.getenv("REDIS_PORT", "6379"))
+    DB = int(os.getenv("REDIS_DB", "0"))
     DECODE_RESPONSES = True
 
     def get(self, key, default=None):
