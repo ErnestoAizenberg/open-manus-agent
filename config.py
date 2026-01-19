8a5597e6d02a71f344b117fc19d30656b32c49c1
Ernesto <sereernest@gmail.com>
2026-01-20 02:32:28 +0300

    Update config.py
    docs: annotate configuration classes

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv("instance/.env")
 
 class Config:
     """Basic Flask config."""
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_very_secret_key_here")
-    DEBUG = os.getenv("FLASK_DEBUG", "false").lower() in ["true", "1", "t"]
-    SERVER_ADDRESS = os.getenv("SERVER_ADDRESS", "https://example.com")
-    HOST = os.getenv("HOST")
-    PORT = int(os.getenv("PORT"))
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "your_very_secret_key_here")
+    DEBUG: bool = os.getenv("FLASK_DEBUG", "false").lower() in ["true", "1", "t"]
+    SERVER_ADDRESS: str = os.getenv("SERVER_ADDRESS", "https://example.com")
+    HOST: str = os.getenv("HOST", "localhost")
+    PORT: int = int(os.getenv("PORT", 9912))
 
     def get(self, key, default=None):
         return getattr(self, key, default)
@@ -19,10 +19,10 @@ class Config:
 
 class RedisConfig:
     """Redis configuration settings."""
-    HOST = os.getenv("REDIS_HOST", "localhost")
-    PORT = int(os.getenv("REDIS_PORT", "6379"))
-    DB = int(os.getenv("REDIS_DB", "0"))
-    DECODE_RESPONSES = True
+    HOST: str = os.getenv("REDIS_HOST", "localhost")
+    PORT: int = int(os.getenv("REDIS_PORT", "6379"))
+    DB: int = int(os.getenv("REDIS_DB", "0"))
+    DECODE_RESPONSES: bool = True
 
     def get(self, key, default=None):
         return getattr(self, key, default)
